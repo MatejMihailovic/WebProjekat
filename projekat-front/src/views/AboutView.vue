3b6b63c5814ec22effd917545e6e4489f32ed778
MatejMihailovic <mihailovicmatej@gmail.com>
2022-06-20 23:12:09 +0200

    Uvezen Bootstrap u Vue

---
+++ projekat-front/src/views/AboutView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <form class="row g-3">
+  <div class="col-md-6">
+    <label for="inputEmail4" class="form-label">Email</label>
+    <input type="email" class="form-control" id="inputEmail4">
+  </div>
+  <div class="col-md-6">
+    <label for="inputPassword4" class="form-label">Password</label>
+    <input type="password" class="form-control" id="inputPassword4">
+  </div>
+  <div class="col-12">
+    <button type="submit" class="btn btn-primary">Sign in</button>
+  </div>
+</form>
+<div class="collapse py-2" id="collapseTarget">
+  This is the toggle-able content!
+</div>
+  </div>
+</template>
